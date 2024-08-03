00c70ee094d269dc3e7ee4712f5c1346d391a08a
CharuKumar01 <kumarcharu83@gmail.com>
2024-08-04 00:18:52 +0530

    react

--- 10todocontextLocal/src/contexts/TodoContext.js
+++ 10todocontextLocal/src/contexts/TodoContext.js
@@ -1,24 +1,60 @@
-/* eslint-disable no-unused-vars */
+import { createContext, useContext, useState } from "react";
 
-import {createContext, useContext} from "react"
+// Initial state
+const initialState = [
+  {
+    id: 1,
+    todo: "Example Todo",
+    completed: false,
+  }
+];
 
+// Create context with initial values
 export const TodoContext = createContext({
-    todos: [
-        {
-            id: 1,
-            todo: " Todo msg",
-            completed: false,
-        }
-    ],
-    addTodo: (todo) => {},
-    updateTodo: (id, todo) => {},
-    deleteTodo: (id) => {},
-    toggleComplete: (id) => {}
-})
+  todos: initialState,
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {}
+});
 
-
+// Custom hook to use the Todo context
 export const useTodo = () => {
-    return useContext(TodoContext)
-}
+  return useContext(TodoContext);
+};
 
-export const TodoProvider = TodoContext.Provider
+// Provider component
+export const TodoProvider = ({ children }) => {
+  const [todos, setTodos] = useState(initialState);
+
+  const addTodo = (todo) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Date.now(), ...todo }
+    ]);
+  };
+
+  const updateTodo = (id, updatedTodo) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => (todo.id === id ? updatedTodo : todo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  return (
+    <TodoContext.Provider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
+      {children}
+    </TodoContext.Provider>
+  );
+};
